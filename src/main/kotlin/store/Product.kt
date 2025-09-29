13c96abf6d70f4b209ac411069a895b47cf19e82
Tail <choijiyoung010@gmail.com>
2025-09-29 23:22:15 +0900

    add: checkProduct

--- src/main/kotlin/store/Product.kt
+++ src/main/kotlin/store/Product.kt
@@ -16,10 +16,12 @@ data class Product(
 )
 
 fun checkProduct(product: Product){
-    var need: String
-    if(product.stock / product.proper <= stockThreshold){
+    var need = 0
+    var inventoryRate = product.stock / product.proper
+    if( inventoryRate <= stockThreshold){
         need = product.proper - product.stock
     }
+    println("${product.name}: 현재 ${product.stock}개 -> 적정재고 ${product.proper} (${need}개 발주 필요) [재고율: $inventoryRate%]")
 }
 
 val stockThreshold = 0.3 // 재고 30% 이하 시 발주 알림
