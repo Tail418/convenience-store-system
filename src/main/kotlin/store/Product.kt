096297f06c4597088f22e76acba37d93e67846e9
Tail <choijiyoung010@gmail.com>
2025-09-29 22:53:15 +0900

    feat: checkProduct

--- src/main/kotlin/store/Product.kt
+++ src/main/kotlin/store/Product.kt
@@ -11,6 +11,15 @@ data class Product(
     val name: String,
     val price: Double,
     val category: ProductCategory,
-    var stock: String,
-    var expiration: Int
+    val proper: String,
+    var stock: String
 )
+
+fun checkProduct(product: Product){
+    var need: String
+    if(product.stock / product.proper <= stockThreshold){
+        need = product.proper - product.stock
+    }
+}
+
+val stockThreshold = 0.3 // 재고 30% 이하 시 발주 알림
