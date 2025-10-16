46a14e70de80669df277761f4cebac4524bb461f
Tail <choijiyoung010@gmail.com>
2025-10-16 22:54:56 +0900

    Fix: JAR 실행 오류 해결 및 Fat JAR 생성 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "bible"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -23,4 +23,19 @@ kotlin {
 
 application{
     mainClass.set("store.MainKt")
+}
+
+// JAR 파일을 만들 때 Manifest 정보와 함께,
+// 실행에 필요한 모든 라이브러리(dependencies)를 포함하도록 설정
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "store.MainKt"
+    }
+    // 이 부분을 추가하거나 기존 내용을 교체합니다.
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(sourceSets.main.get().output)
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
 }
